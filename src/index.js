174588c6e9bd75b481a165970481d3a7251f2c35
Srab <srabmazumdar@gmail.com>
2025-02-01 13:01:10 -0500

    Set up Firebase Auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {initializeApp} from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +14,31 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpuS9bASid-IM8a0gTHS6VeZVvck5F4N8",
+  authDomain: "cineswipe-d6258.firebaseapp.com",
+  projectId: "cineswipe-d6258",
+  storageBucket: "cineswipe-d6258.firebasestorage.app",
+  messagingSenderId: "797181254442",
+  appId: "1:797181254442:web:bc8b6cce3c1b4f98801c53",
+  measurementId: "G-YE0F02BRJ4"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+//Detect auth state
+onAuthStateChanged(auth, user => {
+  if(user != null){
+    console.log('logged in!');
+  } else {
+    console.log('No user');
+  }
+});
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
